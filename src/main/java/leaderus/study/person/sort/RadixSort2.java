c022466b05045accfae8ff7eed3e6e4a1a3223c5
Administrator <Administrator@PC-20151130QUFJ>
2016-03-05 18:26:09 +0800

    优化基数排序

--- src/main/java/leaderus/study/person/sort/RadixSort2.java
+++ src/main/java/leaderus/study/person/sort/RadixSort2.java
@@ -1,70 +1,142 @@
 package leaderus.study.person.sort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class RadixSort2 {
 	
-	private static void radixSort(int[] array,int radix) {
+	private static int [] counter,bucker;
+	
+	private static int length;
+	
+	public static  void sort(int array[],int radix) {
 		
-		int n = 1;// 代表位数对应的数：1,10,100...
+		if((radix & (radix-1)) != 0){ //判断输入是否为2的N次方
+			throw new RuntimeException("请输入2或2的N次方作为基数");
+		}
 		
-		int k = 0;// 保存每一位排序后的结果用于下一位的排序输入
+		int capacity = 1,i=0, max=Integer.MIN_VALUE;
 		
-		int d = radix;
+		int loopCap = 1;
 		
-		int length = array.length;
+		length = array.length;
 		
-		int[][] bucket = new int[radix][length];// 排序桶用于保存每次排序后的结果，这一位上排序结果相同的数字放在同一个桶里
+		counter = new int[radix];
 		
-		int[] order = new int[radix];// 用于保存每个桶里有多少个数字
+		bucker = new int[length];
 		
-		int max = 0;//用于比较最大值与基数,自动增加位数
-		
-		while (n < d) {
+		for(int loop=1;loop<=capacity;loop++){
+			nilOfArray(counter);
 			
-			for (int num : array){ // 将数组array里的每个数字放在相应的桶里
-				int digit = (num / n) % radix; //从低位查找
-				bucket[digit][order[digit]++] = num;
-				max = num>max?num:max;
-			}
-			
-			for (int i = 0; i < radix; i++){// 将前一个循环生成的桶里的数据覆盖到原数组中用于保存这一位的排序结果
-				if (order[i] != 0){// 这个桶里有数据，从上到下遍历这个桶并将数据保存到原数组中
-					for (int j = 0; j < order[i]; j++) {
-						array[k++] = bucket[i][j];
-					}
+			//放入计数时,顺序放入  10,20,30  ==> counter[0] = 1,2,3...
+			//统计边界 桶[0] 的数组边界即为3;
+			//循环数组,将数组放入桶中时,若 正序放入,count[边界-1] = 10 ,与原数组统计计数时的数组位置不符,故需要倒序遍历放入桶中
+			//
+			//计数:					
+			//					0			1		2		3		4	...		
+			//数组下标0	10-->   count+1=1
+			//数组下标1	20-->	count+1=2  
+			//数组下标2	60-->	count+1=3  
+			//数组下标3	30-->	count+1=4
+			//
+			//数组边界:			4
+			//
+			//正序入桶:采用桶边界-1的方式,10-->数组下标为 4-1=3	
+			//倒序入桶:采用通边界-1的方式,30-->[4-1],60-->[3-1],20-->[2-1],10-->[1-1];
+			//
+			for(i=0;i<length;i++){ //循环所得,每个桶中元素的个数
+				int num = getDigit(array[i],radix,loop);
+				counter[num]++;
+				if(loopCap == 1){
+					max = max>array[i]?max:array[i];
 				}
-				order[i] = 0;// 将桶里计数器置0，用于下一次位排序
 			}
 			
-			n *= radix;
-			
-			if(max/n>0){ 
-				d = d*radix;
+			for(i=1;i<radix;i++){ //计算桶中右侧边界值
+				counter[i] = counter[i] + counter[i-1];
 			}
-			k = 0;// 将k置0，用于下一轮保存位排序结果
+			
+			for(i=length-1;i>=0;i--){
+				int num = getDigit(array[i],radix,loop);
+				bucker[counter[num]-1] = array[i];  
+				--counter[num];
+			}
+			
+			for(i = 0;i <= length-1; ++i){
+				array[i] = bucker[i];    
+			}
+			
+			if(loopCap == 1){
+				capacity =  (int) (Math.log(max) / Math.log(radix))+1;
+			}
+			
+			loopCap = 0;
 		}
-
 	}
-
-	/**
-	 * @param 打印桶中的信息
-	 */
-	@SuppressWarnings("unused")
-	private static void printArray(Object [][] temp){
+	
+	private final static void printArray(int [] temp){
+		System.out.println("beg-----------");
 		StringBuffer buff = new StringBuffer();
-		for(Object [] t:temp){
-			for(Object x:t){
-				buff.append(x+" ");
-			}
-			buff.append("\n");
+		for(int t:temp){
+			buff.append(t+" ");
 		}
-		System.out.println(buff);
+		System.out.println(buff+"-----------");
+	}
+	
+	
+	/**
+	 * 获取数字在桶中的索引
+	 * @return
+	 */
+	private static int getDigit(int num,int radix,int rowNum) {
+		return (num / getNom(rowNum,radix)) % radix;
+		
+		//return (num / ( radix * rowNum)) & (radix-1);
+	}
+	
+	private static int getNom(int rownum,int radix){
+		return (int) Math.pow(radix, rownum-1);
+	}
+	
+	/**
+	 * 将数组个元素置为空置
+	 * @param array
+	 */
+	private static void nilOfArray(int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			array [i] = 0; 
+		}
+	}
+	
+	public static void testSort() {
+		int  br[] = {70,20, 80, 90, 589, 998, 965, 852, 123, 456, 789};
+		RadixSort2.sort(br, 10);
+		for (int i : br) {
+			System.out.println(i);
+		}
+	}
+	
+	private static int [] randomArrays(int num) {
+		int [] array = new int[num];
+		for(int i=0;i<num;i++){
+			array[i] = new Random().nextInt(Integer.MAX_VALUE);
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		int [] array = randomArrays(10000000);
+		long start = System.nanoTime();
+		Arrays.sort(array);
+		System.out.println("arrays.sort="+(System.nanoTime()-start));
+		start = System.nanoTime();
+		RadixSort2.sort(array, 512);
+		System.out.println("radixs.sort="+(System.nanoTime()-start));
+		
+//		int  br[] = {70,20, 80, 90, 589, 998, 965, 852, 123, 456, 789};
+//		RadixSort2.sort(br, 16);
+//		printArray(br);
 	}
 
-	public static void main(String[] args) {
-		int[] A = new int[] { 73, 22,25, 93, 343, 55, 14, 28,73, 65, 39, 81 ,1876545902};
-		radixSort(A,12);
-		for (int num : A) {
-			System.out.println(num);
-		}
-	}
+
 }
